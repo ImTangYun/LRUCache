a466d537926f4269b76d9f2886c5baa3fc7fea21
candy <candy@ubuntu.ubuntu-domain>
2014-05-26 19:38:04 +0800

    modified some files

--- lru.h
+++ lru.h
@@ -1,52 +1,27 @@
 #include <stdint.h>
 #include <map>
-#include <queue>
-#include "lru.h"
+#include <deque>
 
 using std::map;
-using std::queue;
+using std::deque;
 
 namespace lru
 {
-//template<typename T>
 #define T int
-LRUCache::LRUCache(uint32_t capacity)
+//template<typename T>
+class LRUCache
 {
-    capacity_ = capacity;
-	size_ = 0;
-}
-uint32_t LRUCache::size() const
-{
-    return size_;
-}
-void LRUCache::Add(const uint32_t &key, const T &vlaue)
-{
-    if (size_ < capacity_) {
-        key_.push_back(key);
-        T *tem = new T(vlaue);
-        cache_[key] = tem;
-		++size_;
-    } else {
-        uint32_t t = key_.front();
-        key_.pop_front();
-        map<uint32_t, T*>::iterator ret = cache_.find(key);
-		if (ret != cache_.end()) {
-			delete ret->second;
-			cache_.erase(t);
-		}
-    }
-}
-int LRUCache::LookUp(uint32_t key, T** value)
-{
-    map<uint32_t, T*>::iterator ret = cache_.find(key);
-    if (ret != cache_.end()) {
-        *value = ret->second;
-        return 0;
-    }
-    return -1;
-}
-void LRUCache::SetCapacity(uint32_t capacity)
-{
-    capacity_ = capacity;
-}
+    public:
+        LRUCache(uint32_t capacity);
+        ~LRUCache(){};
+        int LookUp(uint32_t key, T** value);
+        void Add(const uint32_t &key, const T &vlaue);
+        void SetCapacity(uint32_t capacity);
+        uint32_t size() const;
+    private:
+        deque<uint32_t> key_;
+        map<uint32_t, T*> cache_;
+        uint32_t capacity_;
+        uint32_t size_;
+};
 }
